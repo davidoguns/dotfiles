6507b13026169639610777b6a3534520dd74cff0
David Oguns <davidoguns@gmail.com>
2021-12-25 00:43:53 -0800

    Adding nvim lua configuration with some bare bones plugins.

---
+++ nvim/lua/user/gitsigns.lua
@@ -0,0 +1,18 @@
+require('gitsigns').setup {
+  watch_gitdir = {
+    interval = 5000,
+    follow_files = true
+  },
+  attach_to_untracked = true,
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
+    delay = 500,
+    ignore_whitespace = false,
+  },
+  current_line_blame_formatter_opts = {
+    relative_time = true
+  } 
+}
+
---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,24 @@
+-- :help options to look up what this shit does
+vim.opt.backup = false;
+vim.opt.clipboard = "unnamedplus"
+vim.opt.completeopt = { "menu", "preview" }
+vim.opt.cursorline = true
+vim.opt.mouse = "a"
+vim.opt.showtabline = 2
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.swapfile = false
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 4
+vim.opt.wrap = false
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 4
+vim.opt.signcolumn = "yes"
+vim.opt.background = "light"
+
