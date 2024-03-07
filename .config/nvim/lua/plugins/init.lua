504d97d46bd5c3f0ba50e7755105388d92f73656
David Oguns <davidoguns@gmail.com>
2024-03-06 19:11:01 -0800

    Update web-dev-icons

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,7 +2,7 @@ return {
     { "nvim-lua/popup.nvim" }, -- An implementation of the Popup API from vim in Neovim
     { "nvim-lua/plenary.nvim" }, -- Useful lua functions used ny lots of plugins
 
-    { "kyazdani42/nvim-web-devicons" },
+    { "nvim-tree/nvim-web-devicons" },
 
     { "hrsh7th/cmp-buffer" }, -- Autocompletion plugin
     { "hrsh7th/cmp-path" }, -- Autocompletion plugin
