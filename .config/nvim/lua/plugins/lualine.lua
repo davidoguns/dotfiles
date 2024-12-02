e9447055c1e6d334ff5f2c58815a0003ef407a98
David Oguns <davidoguns@gmail.com>
2024-12-02 01:00:27 -0800

    update: resetting lualine to default config.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,19 +2,30 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "kyazdani42/nvim-web-devicons", opt = true },
     config = function()
-        require('lualine').setup { 
+        require('lualine').setup {
             options = {
                 icons_enabled = true,
                 theme = 'auto',
                 component_separators = { left = '', right = ''},
                 section_separators = { left = '', right = ''},
-                disabled_filetypes = {},
+                disabled_filetypes = {
+                    statusline = {},
+                    winbar = {},
+                },
+                ignore_focus = {},
                 always_divide_middle = true,
+                always_show_tabline = true,
+                globalstatus = false,
+                refresh = {
+                    statusline = 100,
+                    tabline = 100,
+                    winbar = 100,
+                }
             },
             sections = {
                 lualine_a = {'mode'},
                 lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = { {'filename', path = 2 } },
+                lualine_c = {'filename'},
                 lualine_x = {'encoding', 'fileformat', 'filetype'},
                 lualine_y = {'progress'},
                 lualine_z = {'location'}
@@ -28,6 +39,8 @@ return {
                 lualine_z = {}
             },
             tabline = {},
+            winbar = {},
+            inactive_winbar = {},
             extensions = {}
         }
     end
